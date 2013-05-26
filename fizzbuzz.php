7de36eb3d1c7950156157f24ae0891ecf5ef930a
kawamura.hryk <kawamura.hryk@gmail.com>
2013-05-26 15:45:19 +0900

    Add classes and refactor

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -7,6 +7,17 @@ foreach (range(1, $drive) as $i) {
 }
 
 class Party {
+    private static $events = array();
+
+    public function __construct() {
+        $omits = array('Who');
+        foreach (get_declared_classes() as $class) {
+            ! in_array($class, $omits)
+                and method_exists($class, 'say')
+                and static::$events[] = $class;
+        }
+    }
+
     public static function time($new = false) {
         static $time;
         $new and $time = $new;
@@ -19,8 +30,15 @@ class Party {
     }
 
     public function __toString() {
-        $says = fizz::say().Buzz::say();
-        return empty($says) ? (string)static::time() : $says;
+        $says = array();
+        foreach (static::$events as $event) {
+            $say = $event::say();
+            $say and $says[] = $say;
+        }
+        $time = (string)static::time();
+        return empty($says)
+            ? $time
+            : sprintf( '(%d)%s', $time, implode(' ', $says));
     }
 }
 
@@ -35,3 +53,13 @@ abstract class Who {
 class Fizz extends Who { protected static $Ihave = 3; }
 
 class Buzz extends Who { protected static $Ihave = 5; }
+
+class 前執行役員がシフト勤務をします extends Who {
+    protected static $Ihave = 2;
+}
+
+class 河村がお休みをいただきます extends Who {
+    public static function say() {
+        return (gmp_prob_prime(Party::time())) ? get_called_class() : '';
+    }
+}
