72db426b242ea79976582226bae304b18dfdc343
kawamura.hryk <kawamura.hryk@gmail.com>
2013-05-31 00:40:58 +0900

    Add unit tests

--- classes/tests/河村がお休みをいただきます.php
+++ classes/tests/河村がお休みをいただきます.php
@@ -14,18 +14,10 @@ class test_河村がお休みをいただきます extends PHPUnit_Framework_Tes
 		Party::time(1);
 		$this->assertFalse(河村がお休みをいただきます::say());
 		Party::time(2);
-<<<<<<< HEAD
 		$this->assertEquals('河村がお休みをいただきます', 河村がお休みをいただきます::say());
 		Party::time(3);
 		$this->assertEquals('河村がお休みをいただきます', 河村がお休みをいただきます::say());
 		// ... PHP\gmp_prob_prime()のテストをするわけではないのでこんなもんでよかろ
 		
-=======
-        $this->assertEquals('河村がお休みをいただきます', 河村がお休みをいただきます::say());
-		Party::time(3);
-        $this->assertEquals('河村がお休みをいただきます', 河村がお休みをいただきます::say());
-        // ... PHP\gmp_prob_prime()のテストをするわけではないのでこんなもんでよかろ
-        
->>>>>>> 19cc93ef91b25c3f55a4d84e7d090d239bf253ad
 	}
 }
